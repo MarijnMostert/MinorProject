51a50c3bca9a81a6f4461e9d1a898d8b8b159a16
Tom den Ottelander <tdenottelander@gmail.com>
2016-11-16 12:13:53 +0100

    Added gui health, score and death text. Also minor torch bugfix.

--- The Game/Assets/Scripts/CameraController.cs
+++ The Game/Assets/Scripts/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour {
 	public GameObject[] targets;	//A list of targets to follow
 	public Vector3 offset;
 	private Vector3 targetPosition;
+	public Vector3 cameraPosition;
 
 	// Use this for initialization
 	void Start () {
@@ -15,8 +16,14 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		targetPosition = getAveragePosition();
-		transform.position = targetPosition + offset;
+		if (targets.Length != 0) {
+			targetPosition = getAveragePosition ();
+			cameraPosition = targetPosition + offset;
+			transform.position = cameraPosition;
+		} else
+			transform.position = cameraPosition;
+
+		Debug.Log (targets.Length);
 	}
 
 	private Vector3 getAveragePosition(){
--- The Game/Assets/Scripts/Torch.cs
+++ The Game/Assets/Scripts/Torch.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Torch : MonoBehaviour {
@@ -7,40 +8,42 @@ public class Torch : MonoBehaviour {
 	public Light torchLight;
 	public float startingIntensity = 4f;
 	public int health;
-	public float range = 30f;
+	public float range = 5f;
 	public float smoothingTime = 1f;
+	public Text healthText;
+	public Text deathText;
+	[HideInInspector]
+	public bool dead;
 	// public float flickerInterval = 0.5f;
-	private float smoothDampVar = 0f;
+
+
+	private float smoothDampVar1 = 0f;
+	private float smoothDampVar2 = 0f;
 	private float torchLightBase;
 	// private float randomFactor;
 
 	void Start () {
 		torchLight = transform.GetComponentInChildren<Light> ();
 		torchLight.intensity = startingIntensity;
-		torchLightBase = torchLight.intensity;
+		torchLightBase = startingIntensity;
 	//	randomFactor = startingIntensity / 20f;
 
 		health = startingHealth;
+		healthText.text = "Health: " + health;
 
 	//	InvokeRepeating ("torchFlickering", 0f, flickerInterval);
 	}
 	
 	void Update () {
-
 		lightUpdate ();
-
-		if (isDead ()) {
-			CancelInvoke ();
-			Destroy (transform.parent.gameObject);
-		}
 	}
 
 	//Update the light intensity and range according to the health
 	private void lightUpdate(){
 		//Met smoothdamp ga je van de ene waarde geleidelijk over in de andere met een bepaalde smoothingTime.
-		torchLight.range = Mathf.SmoothDamp(torchLight.range, (float)health / startingHealth * range + 20f, ref smoothDampVar, smoothingTime);
+		torchLight.range = Mathf.SmoothDamp(torchLight.range, (float)health / startingHealth * range + 20f, ref smoothDampVar1, smoothingTime);
 		torchLightBase = (float)health / startingHealth * startingIntensity;
-		torchLight.intensity = Mathf.SmoothDamp(torchLight.intensity, torchLightBase, ref smoothDampVar, smoothingTime);
+		torchLight.intensity = Mathf.SmoothDamp(torchLight.intensity, torchLightBase, ref smoothDampVar2, smoothingTime);
 	}
 
 	//Check if the player is dead (health 0 or below)
@@ -53,6 +56,14 @@ public class Torch : MonoBehaviour {
 
 	public void takeDamage(int damage){
 		health -= damage;
+		healthText.text = "Health: " + health;
+
+		if (isDead ()) {
+			dead = true;
+		//	CancelInvoke ();
+			transform.parent.gameObject.SetActive(false);
+			deathText.gameObject.SetActive(true);
+		}
 	}
 
 	/*
