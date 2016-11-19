8a8cd17555700bdcb18f0e8d8766ae023f1563b7
Tom den Ottelander <tdenottelander@gmail.com>
2016-11-19 02:26:55 +0100

    First HomeRoom concept. Working scene loader.

---
+++ The Game/Assets/Scripts/GameStart.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class GameStart : MonoBehaviour {
+
+	public GameObject textObject;
+
+	void Start(){
+		//textObject = Instantiate (textObject, transform.position, transform.rotation) as GameObject;
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.gameObject.CompareTag ("Player")) {
+			textObject.SetActive (true);
+			if (Input.GetButtonDown ("InteractionButton")) {
+				SceneManager.LoadScene ("Scene 1");
+			}
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+			textObject.SetActive (false);
+		}
+	}
+}
---
+++ The Game/Assets/Scripts/TutorialStart.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TutorialStart : MonoBehaviour {
+
+	public GameObject textObject;
+
+	void Start(){
+		//textObject = Instantiate (textObject, transform.position, transform.rotation) as GameObject;
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.gameObject.CompareTag ("Player")) {
+			textObject.SetActive (true);
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if(other.gameObject.CompareTag("Player")){
+			textObject.SetActive (false);
+		}
+	}
+}
