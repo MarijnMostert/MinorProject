c93f9f145f2c6bec05bebe0d99ca3879a730f69b
Tom den Ottelander <tdenottelander@gmail.com>
2016-12-05 17:46:26 +0100

    Added PowerUps system and Shield PowerUp

---
+++ Torch/Assets/Resources/Scripts/PowerUpPickUp.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUpPickUp : MonoBehaviour, IPickUp {
+
+	public GameObject PowerUp;
+
+	public void OnTriggerEnter(Collider collider){
+		if(collider.gameObject.CompareTag("Player")){
+			GameObject player = collider.gameObject;
+			player.GetComponent<Inventory> ().AddItemToInventory (PowerUp, player);
+			Destroy (gameObject);
+		}
+	}
+}
