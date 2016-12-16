1e7980a6c33593cdfce7e690b9003e713bfadf11
DESKTOP-5AQ38OC\siets <smc.lensen@gmail.com>
2016-12-16 14:12:31 +0100

    Update

---
+++ Torch/Assets/Resources/Scripts/PuzzleScripts/Laserbeam.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Laserbeam : MonoBehaviour {
+
+	public float speed;
+	Transform myTransform;
+	public float maxLength;
+	LineRenderer lineRenderer;
+	Vector3 laserVector;
+	RaycastHit hit;
+
+	void Start() {
+		myTransform = transform;
+		lineRenderer = (LineRenderer) GetComponent<LineRenderer>();
+		lineRenderer.SetPosition (1, new Vector3 (0, 0, maxLength));
+	}
+
+	void Update() {
+		myTransform.Rotate (0, speed, 0);
+		UpdateLength ();
+	}
+
+	void UpdateLength(){
+		if (Physics.Raycast (myTransform.position, myTransform.forward, out hit)) {
+			lineRenderer.SetPosition (1, new Vector3 (0, 0, hit.distance));
+
+			if (hit.collider.gameObject.CompareTag("Player")) {
+				KillPlayer ();
+			}
+		} else
+			lineRenderer.SetPosition (1, new Vector3 (0, 0, maxLength));
+	}
+
+	void KillPlayer () {
+		Torch torch = hit.collider.gameObject.GetComponentInChildren <Torch> ();
+		int currentH = torch.health;
+		torch.takeDamage (currentH);
+	}
+
+}
+
+
+
+
+
+
+
+
+
---
+++ Torch/Assets/Resources/Scripts/PuzzleScripts/TriggerActivator.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerActivator : MonoBehaviour {
+
+	public bool fallblock;
+	public float timeleft = 1.0f;
+
+	private bool entered = false;
+
+	void OnTriggerEnter (Collider other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			other.gameObject.transform.SetParent (transform);
+			entered = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.gameObject.CompareTag ("Player")) {
+			transform.DetachChildren();
+		}
+	}
+
+	void Update () {
+		if (fallblock && entered) {
+			timeleft -= Time.deltaTime;
+		}
+		if (fallblock && timeleft < 0) {
+			GetComponentInParent<Rigidbody> ().isKinematic = false;
+		}
+	}
+
+	public void setTimeLeft(float left){
+		timeleft = left;
+		Debug.Log (left);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
