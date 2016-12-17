f06d070b1196a14febc90a003985335315d10d0e
Tom den Ottelander <tdenottelander@gmail.com>
2016-12-17 15:06:56 +0100

    First concept of Object Pooling. Modified hover script.

--- Torch/Assets/Resources/Scripts/Hover.cs
+++ Torch/Assets/Resources/Scripts/Hover.cs
@@ -6,12 +6,17 @@ public class Hover : MonoBehaviour {
 	public float hoverAmplitudeX, hoverSpeedX;
 	public float hoverAmplitudeY, hoverSpeedY;
 	public float hoverAmplitudeZ, hoverSpeedZ;
+	private float startingTime;
+
+	void Start(){
+		startingTime = Time.time;
+	}
 
 	// Update is called once per frame
 	void Update () {
-		float newX = transform.position.x + (1f / 100f * hoverAmplitudeX * Mathf.Sin (Time.time * hoverSpeedX));
-		float newY = transform.position.y + (1f / 100f * hoverAmplitudeY * Mathf.Sin (Time.time * hoverSpeedY));
-		float newZ = transform.position.z + (1f / 100f * hoverAmplitudeZ * Mathf.Sin (Time.time * hoverSpeedZ));
+		float newX = transform.position.x + (1f / 100f * hoverAmplitudeX * Mathf.Sin ((Time.time - startingTime) * hoverSpeedX));
+		float newY = transform.position.y + (1f / 100f * hoverAmplitudeY * Mathf.Sin ((Time.time - startingTime) * hoverSpeedY));
+		float newZ = transform.position.z + (1f / 100f * hoverAmplitudeZ * Mathf.Sin ((Time.time - startingTime) * hoverSpeedZ));
 
 		transform.position = new Vector3(newX, newY, newZ);
 	}
---
+++ Torch/Assets/SetInactive.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetInactive : MonoBehaviour {
+
+	public float TimeTillInactive = 5f;
+
+	void OnEnable(){
+		StartCoroutine (SetInactiveCoroutine ());
+	}
+
+	IEnumerator SetInactiveCoroutine(){
+		yield return new WaitForSeconds (TimeTillInactive);
+		gameObject.SetActive (false);
+	}
+
+}
