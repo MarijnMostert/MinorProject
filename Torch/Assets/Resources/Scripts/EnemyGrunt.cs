f4ac4fce5885f9f7bfb5b9158cff3127a4c08e33
MARIJN\Marijn <marijn-mostert@live.nl>
2016-12-20 16:17:41 +0100

    added spidey

--- Torch/Assets/Resources/Scripts/EnemyGrunt.cs
+++ Torch/Assets/Resources/Scripts/EnemyGrunt.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class EnemyGrunt : Enemy {
 
+	NavMeshAgent agent;
+	Animator animator;
+
 	protected override void Awake(){
 		base.Awake ();
 	}
@@ -11,7 +14,8 @@ public class EnemyGrunt : Enemy {
 	protected override void Start () {
 		base.Start ();
 		StartCoroutine (UpdatePath ());
-
+		agent = GetComponent<NavMeshAgent> ();
+		animator = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -19,14 +23,27 @@ public class EnemyGrunt : Enemy {
 		if (gameManager.enemyTarget != null && distanceToTarget () < attackRange && (Time.time - lastAttackTime) > attackCooldown) {
 			attack ();
 		}
+		if (animator != null) {
+			if (agent.velocity.magnitude > 0.1f) {
+				animator.SetBool ("Walk", true);
+			} else {
+				animator.SetBool ("Walk", false);
+			}
+		}
 	}
 
 	//If the player is close enough to the torch it will do damage
 	private void attack(){
+		if (animator != null) {
+			animator.SetBool ("Attack", true);
+		}
 		IDamagable damagableObject = gameManager.enemyTarget.GetComponent<IDamagable> ();
 		damagableObject.takeDamage (attackDamage, false);
 		//Debug.Log (damagableObject);
 		lastAttackTime = Time.time;
+		if (animator != null) {
+			animator.SetBool ("Attack", false);
+		}
 	}
 
 	//A Coroutine for chasing a target
