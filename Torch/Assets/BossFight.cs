53197d1b775f741a138787d979b4bbe7fe3ac62b
MARIJN\Marijn <marijn-mostert@live.nl>
2016-12-23 14:54:00 +0100

    retarded boss spawns every 5 levels

---
+++ Torch/Assets/BossFight.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossFight : MonoBehaviour {
+
+	public GameObject BossPrefab;
+	private Vector3 spawnpoint;
+	private GameObject BossObject;
+	private GameManager gameManager;
+	bool boss = false;
+
+	// Use this for initialization
+	void Start () {
+		spawnpoint = new Vector3 (transform.position.x, transform.position.y, transform.position.z);
+		gameManager = GameObject.Find ("Game Manager").GetComponent<GameManager> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (gameManager.dungeonLevel % 5 == 0) {
+			if (boss == false) {
+				if (other.gameObject.CompareTag ("Player")) {
+					Debug.Log ("boss instantiated");
+					boss = true;
+					BossObject = Instantiate (BossPrefab);
+					BossObject.transform.position = spawnpoint;
+					BossObject.transform.parent = gameObject.transform;
+				}
+			}
+		}
+	}
+
+}
