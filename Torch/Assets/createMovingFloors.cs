12b3ad9e78f7256441d9d6270f8be09a86dba767
DESKTOP-5AQ38OC\siets <smc.lensen@gmail.com>
2016-12-25 17:45:14 +0100

    Mylever prefab rooms

--- Torch/Assets/createMovingFloors.cs
+++ Torch/Assets/createMovingFloors.cs
@@ -8,13 +8,18 @@ public class createMovingFloors : MonoBehaviour {
 	GameObject platform;
 	List<Vector3> placesx;
 	List<Vector3> placesz;
-
+	List<GameObject> movingblocks;
+	LeverActivator lever { get; set; }
+	bool current = false;
+	public bool finished = false;
 
 	// Use this for initialization
 	void Start () {
+		movingblocks = new List<GameObject> ();
 		blocks = new GameObject("AllPlatforms");
 		blocks.transform.SetParent (transform);
 		platform = Resources.Load("Prefabs/PuzzlesScenes/MovingPlatform", typeof(GameObject)) as GameObject;
+		lever = GetComponent<myLever> ().lever;
 
 		makeList ();
 		makeBlocks ();
@@ -24,16 +29,31 @@ public class createMovingFloors : MonoBehaviour {
 		GetComponentInParent<AllSpeeds> ().Restart ();
 	}
 
+	void Update () {
+		if (!finished && current != lever.is_on && lever.is_on) {
+			finished = true;
+			foreach (GameObject block in movingblocks) {
+				block.SetActive (false);
+			}
+			platform.GetComponent<PingPongPlatform> ().active = false;
+			makeBlocks ();
+			//platform.GetComponent<PingPongPlatform> ().amplitude = 3;
+		}
+		current = lever.is_on;
+	}
+
 	void makeBlocks () {
 		foreach (Vector3 place in placesx) {
 			GameObject floor = GameObject.Instantiate (platform, place, Quaternion.identity, blocks.transform) as GameObject;
 			floor.GetComponent<PingPongPlatform> ().choice = 'z';
+			movingblocks.Add (floor);
 		}
 
 		foreach (Vector3 place in placesz) {
 			Debug.Log (place);
 			GameObject floor = GameObject.Instantiate (platform, place, Quaternion.identity, blocks.transform) as GameObject;
 			floor.GetComponent<PingPongPlatform> ().choice = 'x';
+			movingblocks.Add (floor);
 		}
 	}
 
