5504b8142da6a4515e67fe63577b5783e704e8b5
abovearth <roeland_desmet@hotmail.com>
2016-12-27 14:44:00 +0100

    working look with controller and mouse and mmovement from input manager

--- Torch/Assets/Resources/Scripts/HomeScreenMovement.cs
+++ Torch/Assets/Resources/Scripts/HomeScreenMovement.cs
@@ -10,6 +10,11 @@ public class HomeScreenMovement : MonoBehaviour {
     Vector3 offset;
     float min_height;
 
+	private string moveHorizontal;
+	private string moveVertical;
+	private float HorizontalInput;
+	private float VerticalInput;
+
     Animator anim;
 
     void Awake()
@@ -25,7 +30,8 @@ public class HomeScreenMovement : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-        anim.SetBool("walking", false);
+		Move ();
+        /*anim.SetBool("walking", false);
         if (Input.GetKey("d"))
         {
 			target.transform.Translate(new Vector3(0, 0, walkingSpeed));
@@ -45,16 +51,48 @@ public class HomeScreenMovement : MonoBehaviour {
         {
             target. transform.Translate(new Vector3(walkingSpeed, 0, 0));
             anim.SetBool("walking", true);
-        }
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        }*/
+		float horizontal = Input.GetAxis("turnHorizontal1") * rotateSpeed;
+		if (Mathf.Abs(horizontal) < .1f){//no controllerinput then use mouse
+			horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+		}
         target.transform.Rotate(0, horizontal, 0);
 
         float desiredAngle = target.transform.eulerAngles.y;
-        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed/10f;
+        float vertical = Input.GetAxis("turnVertical1") * rotateSpeed/10f;
+		if (Mathf.Abs(vertical) < .1f){//no controllerinput then use mouse
+			vertical = Input.GetAxis("Mouse Y") * rotateSpeed/10f;
+		}
         Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
         min_height += vertical;
         transform.position = target.transform.position - (rotation * offset) + new Vector3(0,min_height,0);
     }    
+
+	private void Move(){
+		//The input is retrieved and stored
+		HorizontalInput = Input.GetAxis ("moveHorizontal1");
+		VerticalInput = Input.GetAxis ("moveVertical1");
+
+		//Make the player move (Where z-axis is forwards/backwards and x-axis is sideways). No movement in Y-axis.
+		//camera axes relative to player are (-z,y,x)
+		Vector3 MovementInput = new Vector3(-VerticalInput, 0, HorizontalInput);
+
+		//Normalize to account for diagonal walking lines
+		MovementInput = MovementInput.normalized;
+
+		//Move
+		if (anim!=null) {
+			
+			if ((MovementInput * walkingSpeed * Time.deltaTime).magnitude > .1f)
+			{
+				anim.SetBool("walking",true);
+			} else
+			{
+				anim.SetBool("walking", false);
+			}
+		}
+		target.transform.Translate(MovementInput * walkingSpeed * Time.deltaTime);
+	}
 
     void LateUpdate(){
 		transform.LookAt(target.transform);
