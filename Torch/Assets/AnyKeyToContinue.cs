c2023a42ff13e2479aaeeab7180d886515c1d93e
Tom den Ottelander <tdenottelander@gmail.com>
2016-12-27 14:55:31 +0100

    Start screen thingies

---
+++ Torch/Assets/AnyKeyToContinue.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class AnyKeyToContinue : MonoBehaviour {
+
+	private bool boolean = false;
+
+	void Update(){
+		if (Input.anyKeyDown && !boolean) {
+			SceneManager.LoadScene ("create_dungeon");
+			boolean = true;
+		}
+	}
+}
