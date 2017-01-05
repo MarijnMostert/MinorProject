03c57a71f153b7ad9dc1ee9174b4464b43c01082
abovearth <roeland_desmet@hotmail.com>
2017-01-05 17:52:53 +0100

    autostart game deep in cave + bugfix for respawn function

---
+++ Torch/Assets/Resources/Scripts/ColliderAutoStartGame.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ColliderAutoStartGame : MonoBehaviour {
+
+	private GameManager gameManager;
+
+	void Awake(){
+		gameManager = GameObject.Find ("Game Manager").GetComponent<GameManager> ();
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Player")) {
+			gameManager.StartGame ();
+		}
+	}
+}
