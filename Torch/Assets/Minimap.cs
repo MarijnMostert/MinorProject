d0b477c44078712fdc138c930dbf1510b69447c6
MARIJN\Marijn <marijn-mostert@live.nl>
2017-01-12 15:50:53 +0100

    added minimap

---
+++ Torch/Assets/Minimap.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Minimap : MonoBehaviour {
+
+	private GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+
+	void LateUpdate(){
+		gameObject.transform.position = new Vector3 (player.transform.position.x, transform.position.y, player.transform.position.z);
+	}
+		
+		
+}
