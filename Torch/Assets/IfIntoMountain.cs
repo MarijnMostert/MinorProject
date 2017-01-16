4154a9c729030d7ed7d9bdad622dea7c380fd82c
Sietske <s.m.c.lensen@student.tudelft.nl>
2017-01-16 17:10:21 +0100

    Minor Homescreencamfixes

---
+++ Torch/Assets/IfIntoMountain.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfIntoMountain : MonoBehaviour {
+
+	public HomeScreenMovement homescreencam;
+	bool isset = false;
+
+	void OnTriggerStay (Collider other) {
+		GameObject player = other.gameObject;
+		if (player.CompareTag ("Player")) {
+			float ything = player.transform.rotation.eulerAngles.y;
+			Debug.Log (ything);
+			if (ything > 110 && ything < 230) {
+				homescreencam.minimalheight = 4.0f;
+				homescreencam.maximalheight = 5.0f;
+				Debug.Log ("Height set");
+				isset = true;
+			}
+		}
+	}
+
+
+
+	void OnTriggerExit (Collider other) {
+		GameObject player = other.gameObject;
+		if (player.CompareTag ("Player")) {
+			homescreencam.minimalheight = -1.0f;
+			homescreencam.maximalheight = 3.5f;
+			isset = false;
+		}
+	}
+
+}
