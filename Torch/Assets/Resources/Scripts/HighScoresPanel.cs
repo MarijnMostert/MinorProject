5ff47c4ece19638d3ea8e2453e7531c31793daf1
Tom den Ottelander <tdenottelander@gmail.com>
2017-01-16 10:16:05 +0100

    HighScores are now sorted

--- Torch/Assets/Resources/Scripts/HighScoresPanel.cs
+++ Torch/Assets/Resources/Scripts/HighScoresPanel.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
+using System;
 
 public class HighScoresPanel : MonoBehaviour {
 
@@ -10,6 +12,25 @@ public class HighScoresPanel : MonoBehaviour {
 	public Text indexText;
 	public Text namesText;
 	public Text scoresText;
+	public List<HighScore> highScores;
+
+	public class HighScore : IComparable<HighScore>{
+		public string name;
+		public int score;
+
+		public HighScore(string name, int score){
+			this.name = name;
+			this.score = score;
+		}
+
+		public int CompareTo(HighScore other){
+			if (other == null) {
+				return 1;
+			}
+
+			return other.score - this.score;
+		}
+	}
 
 	void Start(){
 		panel.SetActive (false);
@@ -25,13 +46,21 @@ public class HighScoresPanel : MonoBehaviour {
 			data = gameManager.data;
 		}
 
+		highScores = new List<HighScore> ();
+
+		for (int i = 0; i < data.highScoreNames.Count; i++) {
+			highScores.Add (new HighScore (data.highScoreNames [i], data.highScoreScores [i]));
+		}
+
+		highScores.Sort ();
+
 		string indexTemp = "";
 		string namesTemp = "";
 		string scoresTemp = "";
-		for (int i = 0; i < data.highScoreNames.Count; i++) {
+		for (int i = 0; i < highScores.Count; i++) {
 			indexTemp += (i+1) + "\n";
-			namesTemp += data.highScoreNames [i] + "\n";
-			scoresTemp += data.highScoreScores [i] + "\n";
+			namesTemp += highScores [i].name + "\n";
+			scoresTemp += highScores [i].score + "\n";
 		}
 
 		indexText.text = indexTemp;
