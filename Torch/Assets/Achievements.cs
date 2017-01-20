e4015f7423017ddb8ffbfec2a4f729eacded8133
Tom den Ottelander <tdenottelander@gmail.com>
2017-01-20 14:55:55 +0100

    New achievements panel in pause screen (not properly working yet) - Added logout button - Added volume slider - fixed flip room bug - Added arenaPointer

--- Torch/Assets/Achievements.cs
+++ Torch/Assets/Achievements.cs
@@ -100,6 +100,7 @@ public class Achievements : MonoBehaviour {
 		Debug.Log("New Achievement: " + text);
 		Atext.text = "New Achievement: " + text;
 		StartCoroutine ("WaitSecs");
+		AchievementsPanel.Instance.SetAchievement (text);
 	}
 
 	IEnumerator WaitSecs () {
