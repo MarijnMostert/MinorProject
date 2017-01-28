52a6951e5600e961a38ab644549b5e0570f5b00d
Joran Out <joranlol@gmail.com>
2017-01-28 14:04:51 +0100

    temp

--- web/unity/getscoresbyname.php
+++ web/unity/getscoresbyname.php
@@ -9,21 +9,26 @@ $dbhandle = mysql_connect($hostname, $username, $password)
 $selected = mysql_select_db("ewi3620tu1",$dbhandle) 
   or die("Could not select ewi3620tu1");
     
-if(ISSET($_POST["name"])){
+//if(ISSET($_POST["name"])){
 	$name = mysql_real_escape_string($_POST["name"]);
 	
 	//execute the SQL query and return records
 	//mysql_query("");
-	//$result = mysql_query("SET @rank=0; SELECT @rank:=@rank+1 AS rank, name, score FROM score ORDER BY score DESC;");
 	
-	$result = mysql_query("SELECT score.id, score.score, name, score.date FROM score JOIN user ON score.player_id = user.id WHERE name LIKE '%".$name."%'");
+	$result = mysql_query("
+		SELECT h.* FROM (SELECT s.id, s.score, s.date, IF(@score=s.score, @rank:=@rank, @rank:=@rank+1) rank, @score:=s.score score
+		FROM score s,
+		(SELECT @score:=0,@rank:=0) r
+		ORDER BY s.score DESC) h");
+	
+	//$result = mysql_query("SELECT score.id, score.score, name, score.date FROM score JOIN user ON score.player_id = user.id WHERE name LIKE '%".$name."%'");
 	//fetch the data from the database
 	$arr = array();
 	while($obj = mysql_fetch_object($result)) {
 		$arr[] = $obj;
 	}
 	echo '{ "Highscore": '.json_encode($arr).'}';
-} else {
+/*} else {
 	echo "error";
 }
 
